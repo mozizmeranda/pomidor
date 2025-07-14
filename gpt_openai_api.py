e4807a24cad0e5b817fcc428fbd9be80bf64d754
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2025-07-14 23:58:03 +0500

    headless added

--- gpt_openai_api.py
+++ gpt_openai_api.py
@@ -13,7 +13,7 @@ import requests
 
 options = Options()
 options.binary_location = "/home/karraus1/ai-targetolog/chromium/chrome-linux/chrome"
-# options.add_argument("--headless=new")
+options.add_argument("--headless=new")
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
